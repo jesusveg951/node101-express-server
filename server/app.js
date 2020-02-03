0101f1349e08cdf213d3a4f8eaa45c56d3a947d0
Jesus Vega <jesusvega00@gmail.com>
2020-02-03 15:13:13 -0800

    wip

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 // import files and packages up here
-
+var morgan = require('morgan')
+var express = require('express')
+var data = require('data')
 
 // create your express server below
-var app;
+var app = express();
 
 // add your routes and middleware below
-
+app.use(morgan('combined'))
 // finally export the express application
+app.get('/', function(req, res){
+    res.send('data')
+})
 module.exports = app;
