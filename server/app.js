1b6a042f840be07eccbf1d4c98e0bf012912bea6
Jesus Vega <jesusvega00@gmail.com>
2020-02-03 16:13:57 -0800

    finished

--- server/app.js
+++ server/app.js
@@ -1,15 +1,24 @@
 // import files and packages up here
-var morgan = require('morgan')
-var express = require('express')
-var data = require('data')
+const morgan = require('morgan')
+const express = require('express')
+const places = require('./data')
+
+data = (req, res) => {
+    res.send(places);
+}
+
+success = (req, res) => {
+    res.send('Its Working');
+}
 
 // create your express server below
-var app = express();
+const app = express();
 
 // add your routes and middleware below
 app.use(morgan('combined'))
 // finally export the express application
-app.get('/', function(req, res){
-    res.send('data')
-})
+app.use('/', express.static(__dirname), morgan('dev'));
+app.get('/data', data);
+app.get('/', success);
+
 module.exports = app;
